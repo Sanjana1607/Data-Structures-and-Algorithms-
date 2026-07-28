6015c2cf27bbe6d4fceffba760bfead909df3cca
Sanjana1607 <sanjana16aquarius@gmail.com>
2026-07-28 12:41:36 +0530

    Leet-CodeAdd LeetCode 136 - Single Number

--- Arrays/Single_Number.cpp
+++ Arrays/Single_Number.cpp
@@ -2,9 +2,7 @@
 Problem: 136. Single Number
 Link: https://leetcode.com/problems/single-number
 Approach:
-- Initialize ans = 0.
-- Traverse the array.
-- XOR every element with ans.
+- XOR every element of the array.
 - Duplicate elements cancel each other out.
 - The remaining value is the unique element.
 
